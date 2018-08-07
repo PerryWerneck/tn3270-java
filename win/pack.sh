39ddb44534fdae95358dd7addc0f09492de74585
Perry Werneck <perry.werneck@gmail.com>
2018-08-07 16:35:27 -0300

    Incluindo dependências de DLL no pacote da JNI.

--- win/pack.sh
+++ win/pack.sh
@@ -6,25 +6,38 @@ cd $myDIR
 
 rm -f *.zip
 
-win32-configure && make clean && make zip
-if [ "$?" != "0" ]; then
-	exit -1
-fi
+win32() {
 
-win64-configure && make clean && make zip
-if [ "$?" != "0" ]; then
-	exit -1
-fi
+	win32-configure && bash win/makepackage.sh
+	if [ "$?" != "0" ]; then
+		exit -1
+	fi
 
-if [ -d ~/public_html/win/pw3270/x86_32 ]; then
-	cp *i686*.zip ~/public_html/win/pw3270/x86_32
-elif [ -d ~/public_html/win/x86_32 ]; then
-	cp *i686*.zip ~/public_html/win/x86_32
-fi
+	if [ -d ~/public_html/win/pw3270/x86_32 ]; then
+		cp ${myDIR}/*i686*.zip ~/public_html/win/pw3270/x86_32
+	elif [ -d ~/public_html/win/x86_32 ]; then
+		cp ${myDIR}/*i686*.zip ~/public_html/win/x86_32
+	fi
 
-if [ -d ~/public_html/win/pw3270/x86_64 ]; then
-	cp *x86_64*.zip ~/public_html/win/pw3270/x86_64
-elif [ -d ~/public_html/win/x86_32 ]; then
-	cp *x86_64*.zip ~/public_html/win/x86_64
-fi
+}
+
+win64() {
+
+	win64-configure && bash win/makepackage.sh
+	if [ "$?" != "0" ]; then
+		exit -1
+	fi
+
+	if [ -d ~/public_html/win/pw3270/x86_64 ]; then
+		cp -v ${myDIR}/*x86_64*.zip ~/public_html/win/pw3270/x86_64
+	elif [ -d ~/public_html/win/x86_32 ]; then
+		cp -v ${myDIR}/*x86_64*.zip ~/public_html/win/x86_64
+	fi
+
+}
+
+win32
+win64
+
+
 
